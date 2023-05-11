9c9214e716fccd9d941cce5b1c2e4669e76438b9
mocchio <skmt.rome@gmail.com>
2023-05-11 13:51:37 +0900

    コントローラーの編集（登録処理）

--- src/main/java/com/teamlab/startup/todo/controller/TodoController.java
+++ src/main/java/com/teamlab/startup/todo/controller/TodoController.java
@@ -1,10 +1,12 @@
 package com.teamlab.startup.todo.controller;
 
+import com.teamlab.startup.todo.form.TodoForm;
 import com.teamlab.startup.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /** Todo Contoller */
 @Controller
@@ -21,5 +23,16 @@ public class TodoController {
     public String index(Model model) {
         model.addAttribute("todoList", todoService.searchAllTodo());
         return "index";
+    }
+
+    /**
+     * TODO登録処理
+     * @param todoForm 入力値
+     * @return 画面表示用URL
+     */
+    @PostMapping("/register")
+    public String register(TodoForm todoForm) {
+        todoService.saveTodo(todoForm);
+        return "redirect:/";
     }
 }
