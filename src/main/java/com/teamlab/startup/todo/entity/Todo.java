511fb8ad1d5d36880a3abd98768235ab30965d8e
mocchio <skmt.rome@gmail.com>
2023-05-11 13:15:29 +0900

    エンティティの作成

--- src/main/java/com/teamlab/startup/todo/entity/Todo.java
+++ src/main/java/com/teamlab/startup/todo/entity/Todo.java
@@ -1,4 +1,29 @@
 package com.teamlab.startup.todo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/** Todo Entity */
+@Data
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private LocalDate deadline;
+    private boolean status;
+
+    @CreationTimestamp private LocalDateTime createTime;
+    @UpdateTimestamp private LocalDateTime updateTime;
 }
