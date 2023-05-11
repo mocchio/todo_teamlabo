b44ab2edb9ae5e1e324686c154401427b3836509
mocchio <skmt.rome@gmail.com>
2023-05-11 13:25:46 +0900

    サービスクラスの定義

--- src/main/java/com/teamlab/startup/todo/service/TodoService.java
+++ src/main/java/com/teamlab/startup/todo/service/TodoService.java
@@ -1,4 +1,26 @@
 package com.teamlab.startup.todo.service;
 
+import com.teamlab.startup.todo.entity.Todo;
+import com.teamlab.startup.todo.repository.TodoRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/** Todo Service */
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class TodoService {
+    private final TodoRepository todoRepository;
+
+    /**
+     * Todo情報を全件取得する
+     *
+     * @return Todo情報
+     */
+    public List<Todo> searchAllTodo() {
+        return todoRepository.findAll();
+    }
 }
