c8b9e123f175759b275f6cb5a88c7f3cf4bc8060
mocchio <skmt.rome@gmail.com>
2023-05-11 13:32:45 +0900

    コントローラー修正

--- src/main/java/com/teamlab/startup/todo/controller/TodoController.java
+++ src/main/java/com/teamlab/startup/todo/controller/TodoController.java
@@ -1,17 +1,25 @@
 package com.teamlab.startup.todo.controller;
 
+import com.teamlab.startup.todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/** Todo Contoller */
 @Controller
+@RequiredArgsConstructor
 public class TodoController {
+
+    private final TodoService todoService;
     /**
      * トップ画面を表示
-     *
+     * @param model Model
      * @return 画面表示用HTMLパス
      */
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("todoList", todoService.searchAllTodo());
         return "index";
     }
 }
